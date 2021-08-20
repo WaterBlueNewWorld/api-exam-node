517e06091abce657d6b5873b873a60c353f981c7
Alejandro Aviña <alejandroj@tuta.io>
2021-08-20 14:59:18 -0600

    A

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 
 app.all('*', function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Methods','PUT,GET,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods','GET,POST');
     res.header('Access-Control-Allow-Headers','Content-Type');
     next();
 });
